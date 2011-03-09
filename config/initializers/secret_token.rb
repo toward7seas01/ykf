ce1ba6ae9a58a79a33999113e3cf2d6b3f053c70
zhanyinan <zhanyinan@sungomedical.com>
2011-03-09 14:43:01 +0800

    ok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ykf::Application.config.secret_token = 'ae048a7cf5a07ea2dbe4fb8ce357578e93c8c3ad50203ceeb47efe3e55ac07d73860a0c79d315933c2f03e9dd70f37e4e122c18b5499c5c5061d3547642d6669'
